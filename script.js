ec99180bb706e7336dd7a519baa6afa90a535c56
Klaydm <klaydm@gmail.com>
2022-12-01 20:48:36 -0300

    Criando regra para a morte e pontuação, desenhando o criando placar.

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ canoBaixo.src = "images/canoBaixo.png";
 
 //Carregando os sons
 
-fly = new Audio();
+const fly = new Audio();
 fly.src = "sounds/fly.mp3";
-score = new Audio();
-score.src = "sounds/score.mp3";
+const audio = new Audio();
+audio.src = "sounds/score.mp3";
 
 //Variáveis
 
@@ -28,7 +28,7 @@ var constant;
 bX = 33; //Posição X do bird
 bY = 200; //Posição Y do bird
 gravity = 1.4; //Gravidade
-escore = 0; //Placar do jogo
+score = 0; //Placar do jogo
 cano = [];
 
 cano[0] = {
@@ -58,11 +58,27 @@ function jogo() {
         y: Math.floor(Math.random()*canoCima.height)-canoCima.height
       })
     }
+    //Configurando morte do pássaro
+    if(bX + bird.width >= cano[i].x && bX <= cano[i].x + canoCima.width 
+      && (bY <= cano[i].y + canoCima.height || bY + bird.height >= cano[i].y + constant)
+      || bY + bird.height >= canvas.height - chao.height){
+        location.reload();
+    }
+    //Marcando ponto
+    if(cano[i].x == 5){
+      score = score + 1;
+      audio.play();
+    }
 
   }
   ctx.drawImage(chao, 0, canvas.height - chao.height) //Desenhando o chão
   ctx.drawImage(bird, bX, bY); //Desenhando o pássaro
   bY += gravity; //Aplicando a gravidade
+  //Criando o placar do jogo
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Verdana";
+  ctx.fillText("Placar: " + score, 10, canvas.height - 20);
+
   requestAnimationFrame(jogo);  
 }
 
